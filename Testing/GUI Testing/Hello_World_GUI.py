0566c005d27b112d52bcc71d3e01ae1496523fc3
charris561 <89329141+charris561@users.noreply.github.com>
2022-06-29 13:57:19 -0600

    Update Hello_World_GUI.py

---
+++ Testing/GUI Testing/Hello_World_GUI.py
@@ -0,0 +1,9 @@
+from tkinter import *
+from tkinter.ttk import *
+
+root = Tk()
+label = Label(root, text="Hello World")
+button = Button(root, text="I'm a button!")
+label.pack()
+button.pack()
+root.mainloop()
